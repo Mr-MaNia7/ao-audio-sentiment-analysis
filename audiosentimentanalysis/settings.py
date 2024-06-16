62c6f8d6c61de41d71947e33be8faf28a73acfd3
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2024-06-16 14:31:05 +0300

    feat: Update ALLOWED_HOSTS to allow all hosts

--- audiosentimentanalysis/settings.py
+++ audiosentimentanalysis/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3!upw39c^97^ftwl0=s2@s11n-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
